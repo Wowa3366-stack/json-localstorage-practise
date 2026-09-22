cc6de12255d2be1982457f9a9ea561dbd303e6d9
Wowa3366-stack <matkovskyi36@ukr.net>
2026-09-22 19:10:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,31 +23,73 @@
 //   console.log(error);
 // }
 
-
-
 const formEl = document.querySelector(".js-feedback-form");
 const textareaEl = formEl.querySelector('textarea[name="message"]');
 const inputEl = formEl.querySelector('input[name="name"]');
-const FORM_DATA = "form-message"
-const onInputChange = (event)=>{
-// console.log("target", event.target);
-// console.log(event.currentTarget);
-const value = event.target.value;
-localStorage.setItem(FORM_DATA, value);
-}
+const FORM_DATA = "form-data";
+const onInputChange = (event) => {
+  //   console.log(event.target.name, "target");
+  // console.log(event.currentTarget);
 
-formEl.addEventListener("input", onInputChange)
+  const formData = {
+    name: inputEl.value,
+    message: textareaEl.value,
+  };
+  const value = event.target.value;
 
-const populateData = ()=>{
-const data = localStorage.getItem(FORM_DATA);  
-textareaEl.value = data;
-}
+  formData[event.target.name] = value;
+
+  localStorage.setItem(FORM_DATA, JSON.stringify(formData));
+};
+
+formEl.addEventListener("input", onInputChange);
+
+const populateData = () => {
+  const data = localStorage.getItem(FORM_DATA);
+
+  if (data) {
+    const parsedData = JSON.parse(data);
+    console.log(parsedData);
+
+    textareaEl.value = parsedData.message;
+    inputEl.value = parsedData.name;
+  }
+};
 populateData();
 
-const onSubmitForm = (event)=>{
-event.preventDefault();
-event.currentTarget.reset()
-localStorage.removeItem(FORM_DATA)
-}
+const onSubmitForm = (event) => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA);
+};
 
 formEl.addEventListener("submit", onSubmitForm);
+
+// storage.js
+
+function save(key, value) {
+  try {
+    const data = JSON.stringify(value);
+    localStorage.setItem(key, data);
+  } catch (error) {
+    console.log("Set data error", error);
+  }
+}
+
+
+
+function load(key) {
+  try {
+const data = localStorage.getItem(key);
+if (data) {
+    return JSON.parse(data);
+}else{
+    return undefined
+}
+
+  } catch (error) {
+    console.log("Get data error", error);
+  }
+}
+
+
